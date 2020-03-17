cf0ead675b7aeb53642407912cd0443c10343a90
Rafael Fuentes <rafaeltiwifuentes@gmail.com>
2020-03-17 13:52:49 -0400

    Prueba
    Codigo limpio para empezar a practicar lo aprendido

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <button class="mdc-button">
-          <div class="mdc-button__ripple"></div>
-          <span class="mdc-button__label">Button</span>
-    </button>
-    </div>
+    
+      <div>
+        <h1>Hola mundo denuevo xd</h1>
+      </div>
   );
 }
 
